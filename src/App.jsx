c001b8cb7a26dcd5bd360a2702246054f770841a
rumyl <rumyljames@gmail.com>
2023-07-05 10:16:18 +0800

    Latest Changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,18 @@ const App = () => {
   const [city, setCity] = useState('');
   const apiKey = 'c60ad3123b45524f4c2221e366a525c4';
   const [weatherData, setWeatherData] = useState(null);
+  const [temperature, setTemperature] = useState(null);
   const [error, setError] = useState('');
 
   const handleKeyDown = async (event) => {
     if (event.key === 'Enter') {
       try {
         const response = await axios.get(
-          `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+          `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
         );
 
         setWeatherData(response.data);
+        setTemperature(response.data.main.temp);
         setError('');
       } catch (error) {
         setWeatherData(null);
@@ -39,7 +41,7 @@ const App = () => {
         <div>
           <h1>{city}</h1>
           <p>Description: {weatherData.weather[0].description}</p>
-          <p>Temperature: {weatherData.main.temp}°C</p>
+          <p>Temperature: {temperature}°C</p>
           <p>Humidity: {weatherData.main.humidity}%</p>
           <p>Cloudiness: {weatherData.clouds.all}%</p>
         </div>
